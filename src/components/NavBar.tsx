ed9d3a2b9305a79bc2946403a46bd36f1b73911e
tzakari <tzakari@seamfix.com>
2019-12-04 09:53:51 +0100

    created the sidenav and navbar structure

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand navbar-light bg-white px-5">
+                <h4 className="nav-title font-weight-medium">Dashboard</h4>
+
+                <div className="ml-auto">
+
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
